c5e41c51d9608845d532d1e2b79f35f740470455
Allan Silva <allandasilva33@outlook.com>
2025-01-06 19:32:52 -0300

    Estilização do componente Tasks completa

--- src/components/Tasks.jsx
+++ src/components/Tasks.jsx
@@ -1,10 +1,27 @@
-function Tasks(){
+import ChevronRightIcon from '../assets/icons/chevron-right.svg'
+import TrashIcon from '../assets/icons/trash-2.svg'
+
+function Tasks(props){
 
     return (
         <>
-            <div>
-                
-            </div>
+            <section className="w-full p-4 rounded-md shadow-md bg-teal-300">
+                <ul className="flex flex-col gap-4">
+                    {props.propTasks.map((ele)=>(
+                        <li key={ele.id} className='flex items-center gap-2'>
+                            <button className="w-full p-2 text-left rounded-md text-white bg-teal-500">
+                                {ele.title}
+                            </button>
+                            <a href="/details" className='w-full max-w-fit p-2 rounded-md bg-teal-500'>
+                                <img src={ChevronRightIcon} alt="Chevron Right" />
+                            </a>
+                            <button className='w-full max-w-fit p-2 rounded-md bg-teal-500'>
+                                <img src={TrashIcon} alt="Trash" />
+                            </button>
+                        </li>
+                    ))}
+                </ul>
+            </section>
         </>
     )
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,15 +3,35 @@ import Tasks from '../components/Tasks'
 import { useState } from 'react'
 
 function Home(){
-    const [tasks, setTasks] = useState()
-    
+    const [tasks, setTasks] = useState([
+        {
+            id: 1,
+            title: 'Estudar React',
+            description: 'Estudar react todos os dias',
+            checker: false,
+        },
+        {
+            id: 2,
+            title: 'Projeto React',
+            description: 'Projeto publico e Projeto privado',
+            checker: false,
+        },
+        {
+            id: 3,
+            title: 'Estudar Tailwind',
+            description: 'Estudar tailwind em tempo livre',
+            checker: false,
+        },
+    ])
+
+
     return (
         <>
             <main className='w-full min-h-screen flex items-center justify-center bg-teal-50'>
                 <div className='w-full max-w-lg flex flex-col items-center justify-center gap-4'>
                     <h1 className='text-3xl font-bold text-teal-900'>Gerenciador de Tarefas</h1>
                     <TasksAdd />
-                    <Tasks />
+                    <Tasks propTasks={tasks} />
                 </div>
             </main>
         </>
